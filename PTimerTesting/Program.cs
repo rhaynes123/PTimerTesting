0f02d46f4e3cdef29f8060f4190a0ea1842129c8
Richard <rahayn@yahoo.com>
2016-11-19 09:55:04 -0500

    PomodoroTimer
    This is a working test on creating a pomodoro timer. It is a specific
    work technique with 25 minutes of steady focused work then 5 minutes of
    break. I was able to implement 1 round with minor issue but have been
    constantly having issues creating a proper loop.

---
+++ PTimerTesting/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Timers;
+
+public class PomodoroTimer
+{
+	private static System.Timers.Timer myTimer;
+	public static void Main()
+	{
+		SetTimer();
+		Console.WriteLine("\nPress the Enter key to exit the application...\n");
+		Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+		Console.ReadLine();
+		myTimer.Stop();
+		myTimer.Dispose();
+
+	}
+
+	private static void SetTimer()
+	{
+		//Creating a timer with 25 minutes(1500000 miliseconds) interval
+		Console.WriteLine("Its time to start work");
+		myTimer = new System.Timers.Timer(1500000);
+		myTimer.Elapsed += OnTimedEvent;
+		myTimer.AutoReset = true;
+		myTimer.Enabled = true;
+
+
+	}
+	private static void BreakTimer()
+	{
+		//Creating a timer with five minutes interval
+		Console.WriteLine("Your break has begun");
+		myTimer = new System.Timers.Timer(300000);
+		myTimer.Elapsed += OnTimedEvent;
+		myTimer.AutoReset = true;
+		myTimer.Enabled = true;
+
+
+
+	}
+	private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+	{
+		Console.WriteLine("The elapsed event {0:HH:mm:ss.fff}", e.SignalTime);
+		BreakTimer();
+
+
+
+	}
+}
